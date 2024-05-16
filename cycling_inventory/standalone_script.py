20e7febfea9c3ddca00f35fa5b439851c52b7c48
Owen Coombs <owencoombs03@gmail.com>
2024-05-16 17:17:13 -0400

    created new comments and changed update

--- cycling_inventory/standalone_script.py
+++ cycling_inventory/standalone_script.py
@@ -15,13 +15,17 @@ print('SCRIPT START *************************')
 
 from cycling_inv_app.models import Vehicle, Customer, CustomerOrder
 
+
+
+
+
 # Function to display menu options
 def display_menu():
     print("1. Add Vehicle")  # Option to add a new vehicle
-    print("2. Update Vehicle")  # Option to update an existing vehicle
-    print("3. Delete Vehicle")  # Option to delete a vehicle
-    print("4. Show All Vehicles")  # Option to display all vehicles
-    print("5. Exit")  # Option to exit the program
+    print("2. Update Vehicle")  
+    print("3. Delete Vehicle")  
+    print("4. Show All Vehicles")  
+    print("5. Exit") 
 
 # Function to create a new vehicle object and add it to the database
 def create_vehicle(type, number_in_stock):
@@ -31,39 +35,39 @@ def create_vehicle(type, number_in_stock):
 
 # Function to add a new vehicle
 def add_vehicle():
-    type = input("Enter vehicle type (unicycle, bicycle, tricycle): ")  # Prompt user to input vehicle type
-    number_in_stock = int(input("Enter number in stock: "))  # Prompt user to input number of vehicles in stock
-    create_vehicle(type, number_in_stock)  # Call create_vehicle function to add the new vehicle
+    type = input("Enter vehicle type (unicycle, bicycle, tricycle): ")  # choose which type
+    number_in_stock = int(input("Enter number in stock: "))  # how much in stock
+    create_vehicle(type, number_in_stock)  #call create vehicle
 
 # Function to update an existing vehicle
 def update_vehicle():
-    vehicle_id = int(input("Enter vehicle ID to update: "))  # Prompt user to input ID of the vehicle to update
+    vehicle_id = int(input("Enter vehicle ID to update: "))  # input ID of the vehicle to update
     vehicle = Vehicle.objects.get(id=vehicle_id)  # Retrieve the vehicle object from the database
-    type = input("Enter new vehicle type (leave blank to keep current): ")  # Prompt user for new vehicle type
-    number_in_stock = input("Enter new number in stock (leave blank to keep current): ")  # Prompt user for new stock number
+    type = input("Enter new vehicle type (leave blank to keep current): ")  # new vehicle type
+    number_in_stock = input("Enter new number in stock (leave blank to keep current): ")  # amount of stock
     if type:
         vehicle.type = type  # Update vehicle type if user provided a new type
     if number_in_stock:
         vehicle.number_in_stock = number_in_stock  # Update stock number if user provided a new number
     vehicle.save()  # Save the changes to the vehicle object
-    print(f"Vehicle with ID {vehicle_id} updated successfully.")  # Print confirmation message
+    print(f"Vehicle with ID {vehicle_id} updated successfully.")  # print msg
 
 # Function to delete an existing vehicle
 def delete_vehicle():
-    vehicle_id = int(input("Enter vehicle ID to delete: "))  # Prompt user to input ID of the vehicle to delete
-    vehicle = Vehicle.objects.get(id=vehicle_id)  # Retrieve the vehicle object from the database
-    vehicle.delete()  # Delete the vehicle object from the database
-    print(f"Vehicle with ID {vehicle_id} deleted successfully.")  # Print confirmation message
+    vehicle_id = int(input("Enter vehicle ID to delete: "))  # id to delete
+    vehicle = Vehicle.objects.get(id=vehicle_id)  # grabbing vehicle from data
+    vehicle.delete()  # deletes vehicle
+    print(f"Vehicle with ID {vehicle_id} deleted successfully.")  # conf msg
 
 # Function to show all vehicles stored in the database
 def show_all_vehicles():
     vehicles = Vehicle.objects.all()  # Retrieve all vehicle objects from the database
     if vehicles:
-        print("All Vehicles:")  # Print heading for the list of vehicles
+        print("All Vehicles:")  # list of vehicles
         for vehicle in vehicles:
-            print(f"ID: {vehicle.id}, Type: {vehicle.type}, Stock: {vehicle.number_in_stock}")  # Print details of each vehicle
+            print(f"ID: {vehicle.id}, Type: {vehicle.type}, Stock: {vehicle.number_in_stock}")  # details of veh
     else:
-        print("No vehicles found.")  # Print message if no vehicles are found in the database
+        print("No vehicles found.")  # not found
 
 # Main function to run the program
 def main():
